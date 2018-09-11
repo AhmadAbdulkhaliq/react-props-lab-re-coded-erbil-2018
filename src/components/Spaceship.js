494bb518b525f46028dd6b31bf9b349ef1f6d5d1
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-11 08:15:23 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,6 +8,7 @@ class Spaceship extends React.Component {
     return(
       <div>
       <h1>Spaceship Name:  {name} </h1>
+      <p>Ship Info:</p>
       <p>{speed} </p>
       <p>{hasRockets ? 'yes ':'no'} </p>
       <ul>{colors.map((color, index) => <li key={index}>{color}</li>)}</ul>
