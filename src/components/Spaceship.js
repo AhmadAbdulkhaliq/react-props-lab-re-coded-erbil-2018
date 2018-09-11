bfd67a864f34a2c244b14dbdddb478ff185f4390
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-11 08:16:56 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -14,13 +14,13 @@ class Spaceship extends React.Component {
       <ul>{colors.map((color, index) => <li key={index}>{color}</li>)}</ul>
       </div>
       )
-      Spaceship.defaultProps = {
+     
+  }
+  
+}
+ Spaceship.defaultProps = {
   speed: 'slow',
   hasRockets: false,
   colors: ['black', 'red']
 }
-  }
-  
-}
-
 export default Spaceship;
